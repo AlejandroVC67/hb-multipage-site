40284789531084482fbae7899d8cc8fb2801a917
AlejandroVC67 <vcalejandro97@gmail.com>
2018-02-02 15:48:50 -0500

    Now dropdown arrows have a css animation

--- src/model/NavBar/main.js
+++ src/model/NavBar/main.js
@@ -4,21 +4,37 @@ export class NavBarModel {
     this.elements = {}
     this.elements.menu = this.node.querySelector('.nav-bar__menu__button')
     this.elements.displayer = this.node.querySelector('.nav-bar__list')
+    this.elements.dropDownButton = this.node.querySelectorAll('.nav-bar__list-item-container-button')
+    this.elements.dropDownList = this.node.querySelectorAll('.nav-bar__dropdown-list')
     this.setMenuAction(this.elements.menu, this.elements.displayer)
+    this.setDropDownAction(this.elements.dropDownButton, this.elements.dropDownList)
   }
   static states () {
     return {
       displayerActive: (`nav-bar__list--active`)
     }
   }
+
   setMenuAction (menuButton, itemsDisplayer) {
-    console.log(NavBarModel.states.displayerActive)
     menuButton.addEventListener('click', () => {
       itemsDisplayer.classList.toggle('nav-bar__list--active')
     })
   }
 
-//   displayOptions (itemsDisplayer) {
-//     itemsDisplayer.classList.toggle('nav-bar__list--active')
-//   }
+  setDropDownAction (dropDownButton) {
+    dropDownButton.forEach(element => {
+      element.addEventListener('click', () => {
+        const index = Array.from(dropDownButton).indexOf(element)
+        this.changeArrows(element, index)
+        this.elements.dropDownList[index].classList.toggle('nav-bar__dropdown-list--active')
+      })
+    })
+  }
+
+  changeArrows (element, index) {
+    element.children[1].children[0].classList.toggle('leftArrow--active')
+    element.children[1].children[1].classList.toggle('rightArrow--active')
+    console.log(element.children[1].children[0])
+    console.log(element.children[1].children[1])
+  }
 }
