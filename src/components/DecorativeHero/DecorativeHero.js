7a5c5e17a7c3e86aba22e474adbaeed555bd3773
AlejandroVC67 <vcalejandro97@gmail.com>
2018-02-23 12:23:52 -0500

    added windowlistener

--- src/components/DecorativeHero/DecorativeHero.js
+++ src/components/DecorativeHero/DecorativeHero.js
@@ -1,9 +1,18 @@
 import './_DecorativeHero.scss'
 import template from './DecorativeHero.pug'
 import data from '../../Data/data.json'
+import { WindowListener } from '../common/windowListener.js'
+
+function doSomething (breakpoint) {
+  console.log(breakpoint)
+}
+
 export class DecorativeHero {
   constructor (node) {
     this.node = node
+    /* eslint-disable */    
+    new WindowListener(doSomething)
+    /* eslint-enable */
     this.generateHTML()
   }
 
