ba3916d2404e7196b585be90c067625ad4911845
AlejandroVC67 <vcalejandro97@gmail.com>
2018-02-23 13:05:06 -0500

    Finished DecorativeHero

--- src/components/DecorativeHero/DecorativeHero.js
+++ src/components/DecorativeHero/DecorativeHero.js
@@ -3,20 +3,34 @@ import template from './DecorativeHero.pug'
 import data from '../../Data/data.json'
 import { WindowListener } from '../common/windowListener.js'
 
-function doSomething (breakpoint) {
-  console.log(breakpoint)
-}
-
 export class DecorativeHero {
   constructor (node) {
     this.node = node
-    /* eslint-disable */    
-    new WindowListener(doSomething)
-    /* eslint-enable */
     this.generateHTML()
+    /* eslint-disable */    
+    new WindowListener(this.addHeroStyles)
+    /* eslint-enable */
   }
-
   generateHTML () {
     this.node.innerHTML = template(data.decorativehero)
+  }
+
+  addHeroStyles (breakpoint) {
+    this.images = {}
+    this.images.small = data.decorativehero.images.small.url
+    this.images.medium = data.decorativehero.images.medium.url
+    this.images.large = data.decorativehero.images.large.url
+    this.decorativeHero = document.querySelector('.decorative-hero__picture')
+    console.log(breakpoint)
+    console.log(this.decorativeHero)
+    console.log(this.images)
+
+    if (breakpoint === 'small') {
+      this.decorativeHero.setAttribute('style', `background-image: url(${this.images.small})`)
+    } else if (breakpoint === 'medium') {
+      this.decorativeHero.setAttribute('style', `background-image: url(${this.images.medium})`)
+    } else if (breakpoint === 'large') {
+      this.decorativeHero.setAttribute('style', `background-image: url(${this.images.large})`)
+    }
   }
 }
--- src/components/common/windowListener.js
+++ src/components/common/windowListener.js
@@ -1,36 +1,30 @@
-/* eslint-disable */
 export class WindowListener {
-    constructor (callback) {
-        this.callback = callback
-        this.elements = {}
-        this.initVariables()
-        this.setEvents(this.callback)
-    }
+  constructor (callback) {
+    this.callback = callback
+    this.elements = {}
+    this.initVariables()
+    this.setEvents(this.callback)
+  }
 
-    initVariables () {
-        this.elements.small = window.matchMedia('screen and (min-width: 320px) and (max-width: 630px) ')
-        this.elements.medium = window.matchMedia('screen and (min-width: 640px) and (max-width: 1023px)')
-        this.elements.large = window.matchMedia('screen and (min-width: 1024px)')
-    }
+  initVariables () {
+    this.elements.small = window.matchMedia('screen and (min-width: 320px) and (max-width: 630px) ')
+    this.elements.medium = window.matchMedia('screen and (min-width: 640px) and (max-width: 1023px)')
+    this.elements.large = window.matchMedia('screen and (min-width: 1024px)')
+  }
 
-    setEvents (cb) {
-        this.elements.small.addListener(this.getCurrentWidth.bind(this))
-        this.elements.medium.addListener(this.getCurrentWidth.bind(this))
-        this.elements.large.addListener(this.getCurrentWidth.bind(this))
+  setEvents (cb) {
+    this.elements.small.addListener(this.getCurrentWidth.bind(this))
+    this.elements.medium.addListener(this.getCurrentWidth.bind(this))
+    this.elements.large.addListener(this.getCurrentWidth.bind(this))
+  }
+
+  getCurrentWidth () {
+    if (this.elements.small.matches && window.innerWidth >= 320 && window.innerWidth < 639) {
+      this.callback('small')
+    } else if (this.elements.medium.matches && window.innerWidth >= 640 && window.innerWidth < 1024) {
+      this.callback('medium')
+    } else if (this.elements.large.matches && window.innerWidth >= 1024) {
+      this.callback('large')
     }
-    
-    getCurrentWidth () {
-          if (this.elements.small.matches && window.innerWidth >= 320 && window.innerWidth < 639) {
-            this.callback('small')
-          } else if (this.elements.medium.matches &&window.innerWidth >= 640 && window.innerWidth < 1024) {
-            this.callback('medium')
-          } else if ( this.elements.large.matches && window.innerWidth >= 1024) {
-            this.callback('large')
-          }
-      }
+  }
 }
-
-
-
-
-/* eslint-enable */
